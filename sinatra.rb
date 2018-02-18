e94d17715335c42649f0b84e7d0aa6729c634912
Astrid Varga <astrid.varga@gmx.de>
2018-02-18 13:35:52 +0100

    Added /views and modified controller (sinatra.rb) correspondingly

--- sinatra.rb
+++ sinatra.rb
@@ -1,21 +1,10 @@
 require 'sinatra'
-  
+
 $moodsum = 0
 
-  get '/' do 
-    %{
-    <form method="post">
-    <input type="radio" name="mood" value= 1> very bad<br>
-    <input type="radio" name="mood" value= 2> bad<br>
-    <input type="radio" name="mood" value= 3> neutral<br>
-    <input type="radio" name="mood" value= 4> good<br>
-    <input type="radio" name="mood" value= 5> very good<br>
-    Anything to add?<br>
-    <input type="text" placeholder="reason for your mood" name="reason-for-mood"><br>
-    <button type="submit">submit</button>
-    </form>
-  }
-  end
+get '/' do 
+  erb :index
+end
 
 post '/' do
   puts params
@@ -23,8 +12,4 @@ post '/' do
   puts $moodsum
   "Thank you!"
 end
-
-  get '/frank-says' do
-      'Put this in your pipe & smoke it!'
-    end
-    
+ 
