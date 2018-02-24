8da46b6bc64eea7a506c1387bb1e4200f36e777c
davidcoroian <david.coroian@code.berlin>
2018-02-24 15:30:26 +0100

    fourth try

--- config/environments.rb
+++ config/environments.rb
@@ -1,2 +1,2 @@
-#DB = Sequel.connect(ENV['DATABASE_URL'] || 'postgres://localhost/postgres')
-DB = Sequel.connect('postgres://david:david@localhost/moodsdatabase')
+DB = Sequel.connect(ENV['DATABASE_URL'] || 'postgres://david:david@localhost/moodsdatabase')
+#DB = Sequel.connect('postgres://david:david@localhost/moodsdatabase')
