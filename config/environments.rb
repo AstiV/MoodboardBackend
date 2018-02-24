e98d34f1080daefe05782c16f474476fd67e2b40
davidcoroian <david.coroian@code.berlin>
2018-02-24 14:42:20 +0100

    added database functionality

--- config/environments.rb
+++ config/environments.rb
@@ -1,1 +1,2 @@
-DB = Sequel.connect(ENV['DATABASE_URL'] || 'postgres://localhost/Moods')
+#DB = Sequel.connect(ENV['DATABASE_URL'] || 'postgres://localhost/postgres')
+DB = Sequel.connect('postgres://david:david@localhost/moodsdatabase')
