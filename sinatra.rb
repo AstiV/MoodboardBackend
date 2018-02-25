fdb616c49b676174e21089a5f4baf501c1552ddd
davidcoroian <david.coroian@code.berlin>
2018-02-25 18:21:27 +0100

    optimising

--- sinatra.rb
+++ sinatra.rb
@@ -2,18 +2,13 @@ require 'sinatra'
 require 'sequel'
 require 'pg'
 require './config/environments' #database configuration
-#DB = Sequel.connect('postgres://david:david@localhost/moodsdatabase')
-#DB = Sequel.postgres('moodsdatabase',:user=>'david',:password=>'david',:host=>'localhost',:port=>5432,:max_connections=>10)
-$moodsum = 0
-#moods = DB[:moodsdatabase]
 
 get '/' do 
   erb :index
 end
 
 post '/' do
-  puts params
-  $moodsum = $moodsum + params["mood"].to_i
+  #puts params
   DB[:moodsdata].insert(value: params["mood"].to_i)
   erb :result
 end
